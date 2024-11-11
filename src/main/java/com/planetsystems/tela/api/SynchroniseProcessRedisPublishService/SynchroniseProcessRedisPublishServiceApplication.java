52f6af6da8a072b18b771e13c24358caa0503076
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-11 23:59:34 +0300

    initial configuration

---
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/SynchroniseProcessRedisPublishServiceApplication.java
@@ -0,0 +1,13 @@
+package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchroniseProcessRedisPublishServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchroniseProcessRedisPublishServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/SynchroniseProcessRedisPublishServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchroniseProcessRedisPublishServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
