6ef2fb251c54250899ed1bb8eb68d7a807b41e1f
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-12 11:02:00 +0300

    added SynchronizeMobileDataService with following services
        ResponseEntity<Boolean> synchronizeSchoolData(SynchronizeSchoolDataDTO dto);
    
        void publishSchoolData(SynchronizeSchoolDataDTO dto);
        void publishSchoolClasses(SchoolDTO schoolDTO) throws JsonProcessingException;
        void publishSchoolStaffs(SchoolDTO schoolDTO) throws JsonProcessingException;
        void publishSchoolClockIns(SchoolDTO schoolDTO, String dateParam) throws JsonProcessingException;
        void publishSchoolClockOuts(SchoolDTO schoolDTO , String dateParam) throws JsonProcessingException;
        void publishSubjects(SchoolDTO schoolDTO) throws JsonProcessingException;
        void publishLearnerEnrollments(SchoolDTO schoolDTO) throws JsonProcessingException;
        void publishLearnerAttendance(SchoolDTO schoolDTO,  String dateParam) throws JsonProcessingException;
        void publishStaffDailyTimeAttendanceSupervision(SchoolDTO schoolDTO, String dateParam) throws JsonProcessingException;
        void publishStaffDailyTimetableTaskSupervision(SchoolDTO schoolDTO, String dateParam) throws JsonProcessingException;
    
        void publishStaffDailyTimetables(SchoolDTO schoolDTO , String dateParam) throws JsonProcessingException;
        void publishDistricts();
        void publishSchoolTimetables(SchoolDTO schoolDTO) throws JsonProcessingException;

---
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/service/publisher/SynchronizeMobileDataService.java
@@ -0,0 +1,32 @@
+package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.service.publisher;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto.SchoolDTO;
+import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto.SynchronizeSchoolDataDTO;
+import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.model.AcademicTerm;
+import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.model.School;
+import org.springframework.http.ResponseEntity;
+
+public interface SynchronizeMobileDataService {
+    ResponseEntity<Boolean> synchronizeSchoolData(SynchronizeSchoolDataDTO dto);
+
+
+
+    void publishSchoolData(SynchronizeSchoolDataDTO dto);
+    void publishSchoolClasses(SchoolDTO schoolDTO) throws JsonProcessingException;
+    void publishSchoolStaffs(SchoolDTO schoolDTO) throws JsonProcessingException;
+    void publishSchoolClockIns(SchoolDTO schoolDTO, String dateParam) throws JsonProcessingException;
+    void publishSchoolClockOuts(SchoolDTO schoolDTO , String dateParam) throws JsonProcessingException;
+    void publishSubjects(SchoolDTO schoolDTO) throws JsonProcessingException;
+    void publishLearnerEnrollments(SchoolDTO schoolDTO) throws JsonProcessingException;
+    void publishLearnerAttendance(SchoolDTO schoolDTO,  String dateParam) throws JsonProcessingException;
+    void publishStaffDailyTimeAttendanceSupervision(SchoolDTO schoolDTO, String dateParam) throws JsonProcessingException;
+    void publishStaffDailyTimetableTaskSupervision(SchoolDTO schoolDTO, String dateParam) throws JsonProcessingException;
+
+
+    void publishStaffDailyTimetables(SchoolDTO schoolDTO , String dateParam) throws JsonProcessingException;
+    void publishDistricts();
+    void publishSchoolTimetables(SchoolDTO schoolDTO) throws JsonProcessingException;
+
+
+}
