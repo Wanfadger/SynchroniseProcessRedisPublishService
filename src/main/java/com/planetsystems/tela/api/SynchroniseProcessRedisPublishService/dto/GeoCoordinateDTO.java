9b0d34a53a64847953b09899101c7c02c4563c57
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-12 10:56:16 +0300

    implemented serializable to enable caching

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/dto/GeoCoordinateDTO.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/dto/GeoCoordinateDTO.java
@@ -2,13 +2,15 @@ package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @ToString
-public class GeoCoordinateDTO {
+public class GeoCoordinateDTO implements Serializable {
     private String id;
     double latitude;
     double longitude;
