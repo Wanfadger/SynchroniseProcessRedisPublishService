0461b3a71259124929171bd341c8afd43baaa3ab
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-12 11:00:23 +0300

    added EnableCaching

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/SynchroniseProcessRedisPublishServiceApplication.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/SynchroniseProcessRedisPublishServiceApplication.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @Slf4j
 @RequiredArgsConstructor
+@EnableCaching
 public class SynchroniseProcessRedisPublishServiceApplication {
 
 	public static void main(String[] args) {
