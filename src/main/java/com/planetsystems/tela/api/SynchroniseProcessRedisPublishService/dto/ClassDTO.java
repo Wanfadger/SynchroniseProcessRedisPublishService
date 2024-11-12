0640874a8ce19e94c9d0b0a330c5abfe950184cd
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-12 10:55:51 +0300

    implemented serializable to enable caching

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/dto/ClassDTO.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/dto/ClassDTO.java
@@ -2,13 +2,15 @@ package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @ToString
-public class ClassDTO {
+public class ClassDTO implements Serializable {
     private String id;
     private String name;
     private int localId;
