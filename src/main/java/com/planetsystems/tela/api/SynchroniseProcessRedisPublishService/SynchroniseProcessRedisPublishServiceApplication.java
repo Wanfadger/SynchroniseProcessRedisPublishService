ff1dabada40e86f1a7cb0cbf0e2b3b67d480570c
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-20 10:03:12 +0300

    quick test

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/SynchroniseProcessRedisPublishServiceApplication.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/SynchroniseProcessRedisPublishServiceApplication.java
@@ -2,8 +2,10 @@ package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,10 +14,20 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Slf4j
 @RequiredArgsConstructor
 @EnableCaching
-public class SynchroniseProcessRedisPublishServiceApplication {
+public class SynchroniseProcessRedisPublishServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SynchroniseProcessRedisPublishServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		String telaNumber = "8008229464166";
+		evictSchoolData(telaNumber);
+	}
+
+	@CacheEvict(value = "SCHOOL" , key = "#telaSchoolNumber")
+	public void evictSchoolData(String telaSchoolNumber) {
+		log.info("evictSchoolData {}" , telaSchoolNumber );
+	}
 }
