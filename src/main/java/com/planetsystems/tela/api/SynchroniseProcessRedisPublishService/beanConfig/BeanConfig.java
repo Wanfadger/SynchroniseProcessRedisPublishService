10c3312c504104c4550d25a06e8c6c18ae6cbc9d
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-22 16:49:07 +0300

    quick fix

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/beanConfig/BeanConfig.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/beanConfig/BeanConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
-
 	@Bean
 	public ObjectMapper objectMapper(){
 		return new ObjectMapper();
