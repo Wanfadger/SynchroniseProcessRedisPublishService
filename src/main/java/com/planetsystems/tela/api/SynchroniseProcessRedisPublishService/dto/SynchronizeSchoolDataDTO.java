03f82a604528b539e2789e8c4c41b6eb101fde52
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-22 16:48:03 +0300

    implemented Serializable

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/dto/SynchronizeSchoolDataDTO.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/dto/SynchronizeSchoolDataDTO.java
@@ -1,5 +1,7 @@
 package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto;
 
 
-public record SynchronizeSchoolDataDTO(String telaSchoolNumber , String date) {
+import java.io.Serializable;
+
+public record SynchronizeSchoolDataDTO(String telaSchoolNumber , String date) implements Serializable {
 }
