aaa6511c65121d873f8061f2f5a5fdc8339f2706
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-22 16:47:20 +0300

    added test evictDistrictService

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/controllers/SynchroniseTelaDataController.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/controllers/SynchroniseTelaDataController.java
@@ -2,23 +2,46 @@ package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.control
 
 import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto.SynchronizeSchoolDataDTO;
 import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto.SystemAppFeedBack;
+import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.service.cache.CacheKeys;
 import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.service.publisher.SynchronizeMobileDataService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.interceptor.SimpleKey;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/SynchroniseTelaData")
 @RequiredArgsConstructor
+@Slf4j
 public class SynchroniseTelaDataController {
 
     final SynchronizeMobileDataService synchronizeMobileDataService;
 
+
+    @Qualifier("halfHourCacheManager")
+    final CacheManager cacheManager;
+
     @PostMapping
     public ResponseEntity<SystemAppFeedBack<Boolean>> synchronizeSchoolData(@RequestBody SynchronizeSchoolDataDTO dto){
         return synchronizeMobileDataService.synchronizeSchoolData(dto);
+    }
+
+
+    @GetMapping("evictDistricts")
+    public String evictDistricts(){
+        evictDistrictService();
+        return "Successfully evicted";
+    }
+
+    public void evictDistrictService() {
+        synchronizeMobileDataService.evictDistricts();
+        // Optional.ofNullable(cacheManager.getCache(CacheKeys.DISTRICTS)).ifPresent((cache -> cache.evict(SimpleKey.EMPTY) ));
+        log.info("CACHE EVICTING evictDistricts");
     }
 }
