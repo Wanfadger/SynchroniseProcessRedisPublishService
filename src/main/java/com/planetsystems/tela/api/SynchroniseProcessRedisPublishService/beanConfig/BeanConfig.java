e241dfd71c024b31ed25e92b532aeaaef46f01f0
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-28 10:07:45 +0300

    quick fix

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/beanConfig/BeanConfig.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/beanConfig/BeanConfig.java
@@ -3,12 +3,16 @@ package com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.beanCon
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class BeanConfig {
 	@Bean
+	@Primary
 	public ObjectMapper objectMapper(){
 		return new ObjectMapper();
 	}
+
+
 
 }
