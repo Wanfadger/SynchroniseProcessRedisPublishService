b541d48bdafa329a313df50d46716b68aa770f6e
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:08:44 +0300

    cleaned StaffDailyTimeTableLessonRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/StaffDailyTimeTableLessonRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/StaffDailyTimeTableLessonRepository.java
@@ -19,38 +19,4 @@ public interface StaffDailyTimeTableLessonRepository extends JpaRepository<Staff
             AND SDTL.staffDailyTimeTable IN :staffDailyTimeTables
             """)
     List<StaffDailyTimeTableLesson> allIn(List<StaffDailyTimeTable> staffDailyTimeTables);
-
-//    List<StaffDailyTimeTableLesson> findAllByStatusNot(Status status);
-//    List<StaffDailyTimeTableLesson> findAllByStatus(Status status);
-//
-//    Optional<StaffDailyTimeTableLesson> findByStatusNotAndId(Status status , String id);
-//
-//    @EntityGraph(value = "staff-daily-timeTable-lesson-detail-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<StaffDailyTimeTableLesson> findAllByStatusNotAndStaffDailyTimeTable_SchoolStaff_IdAndLessonDate(Status status , String staffId , LocalDate lessonDate);
-//
-//    @EntityGraph(value = "staff-daily-timeTable-lesson-detail-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<StaffDailyTimeTableLesson> findAllByStatusNotAndSchoolClass_School_IdAndLessonDate(Status status , String staffId , LocalDate lessonDate);
-//
-//    @EntityGraph(value = "staff-daily-timeTable-lesson-detail-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<StaffDailyTimeTableLesson> findAllByStatusNotAndStaffDailyTimeTable_IdAndStaffDailyTimeTable_SchoolStaff_IdAndLessonDate(Status status , String staffDailyTimetableId , String staffId , LocalDate lessonDate);
-//
-//    @EntityGraph(value = "staff-daily-timeTable-lesson-detail-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<StaffDailyTimeTableLesson> findByStatusNotAndStaffDailyTimeTable_SchoolStaff_IdAndSchoolClass_IdAndSubject_IdAndLessonDate
-//    (Status status , String staffId , String classId , String subjectId , Date lessonDate);
-//
-//
-//    @EntityGraph(value = "staff-daily-timeTable-lesson-detail-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<StaffDailyTimeTableLesson> findAllByStatusNotAndStaffDailyTimeTable_SchoolStaff_IdAndSchoolClass_IdAndSubject_IdAndLessonDate
-//            (Status status , String staffId , String classId , String subjectId , Date lessonDate);
-//
-//    Optional<StaffDailyTimeTableLesson> findByStatusNotAndStaffDailyTimeTable_SchoolStaff_IdAndSchoolClass_IdAndSubject_IdAndLessonDateAndStartTimeAndEndTime
-//            (Status status , String staffId , String classId , String subjectId , Date lessonDate, LocalTime startTime,
-//      LocalTime endTime);
-//
-//
-//    List<StaffDailyTimeTableLesson> findAllByStatusNotAndStaffDailyTimeTable_SchoolStaff_IdAndSchoolClass_IdAndSubject_IdAndLessonDateAndStartTimeAndEndTime
-//            (Status status , String staffId , String classId , String subjectId , Date lessonDate, LocalTime startTime,
-//             LocalTime endTime);
-//
-
 }
