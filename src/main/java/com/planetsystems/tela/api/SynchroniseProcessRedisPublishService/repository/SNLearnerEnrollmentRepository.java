2ed0ce8a2766702351ced700c58cb50837ed12bf
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:06:33 +0300

    cleaned SNLearnerEnrollmentRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SNLearnerEnrollmentRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SNLearnerEnrollmentRepository.java
@@ -17,21 +17,4 @@ public interface SNLearnerEnrollmentRepository extends JpaRepository<SNLearnerEn
             AND SC.school.id =:schoolId AND SC.academicTerm.id =:termId
             """)
     List<SNLearnerEnrollment> allBySchool_term(String schoolId, String termId);
-
-//    boolean existsByStatusNotAndSchoolStaff_Id(Status status , String classId);
-//
-//    Optional<SNLearnerEnrollment> findByStatusNotAndSchoolClass_Id(Status status , String classId);
-//
-//    List<SNLearnerEnrollment> findAllByStatusNot(Status status);
-//    Optional<SNLearnerEnrollment> findByStatusNotAndId(Status status , String id);
-//
-//    boolean existsByStatusNotAndSchoolClass_Id(Status status , String schoolClassId);
-//    List<SNLearnerEnrollment> findAllByStatusNotAndSchoolClass_AcademicTerm_IdAndSchoolClass_School_Id(Status status , String termId , String schoolId);
-//    List<SNLearnerEnrollment> findAllByStatusNotAndSchoolClass_AcademicTerm_Id(Status status , String termId);
-//    List<SNLearnerEnrollment> findAllByStatusNotAndSchoolClass_AcademicTerm_IdAndSchoolClass_School_District_Region_Id(Status status , String termId , String regionId);
-//
-//    List<SNLearnerEnrollment> findAllByStatusNotAndSchoolClass_AcademicTerm_IdAndSchoolClass_School_District_Id(Status status , String termId , String districtId);
-//
-//
-
 }
