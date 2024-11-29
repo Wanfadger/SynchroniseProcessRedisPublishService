670a84f243da16214d2e15e622f846302d41af51
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:09:50 +0300

    cleaned SubjectRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SubjectRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SubjectRepository.java
@@ -11,23 +11,5 @@ import java.util.List;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, String> {
-
     List<Subject> findAllBySubjectClassificationNotNullAndStatusNotAndSubjectClassification(Status status , SubjectClassification subjectClassification);
-
-//
-//    List<Subject> findAllByStatusNot(Status status);
-//    List<Subject> findAllByStatus(Status status);
-//    Optional<Subject> findByStatusNotAndId(Status status , String id);
-//    Optional<Subject> findByStatusNotAndCode(Status status , String code);
-//
-//    // Checks if a subject exits in category
-//
-//    List<Subject> findAllByStatusAndSubjectCategory_Id(Status status , String categoryId);
-//    List<Subject> findAllByStatusNotAndSubjectClassification(Status status , SubjectClassification classification);
-//
-//    boolean existsByStatusNotAndNameIgnoreCaseAndSubjectCategory_Id(Status deleted, String name ,String categoryId);
-//    boolean existsByStatusNotAndNameIgnoreCaseAndSubjectClassificationAndSubjectCategory_Id(Status deleted, String name , SubjectClassification classification ,  String categoryId);
-//
-//    List<Subject> findAllByStatusNotAndSubjectClassificationNotNull(Status status);
-
 }
