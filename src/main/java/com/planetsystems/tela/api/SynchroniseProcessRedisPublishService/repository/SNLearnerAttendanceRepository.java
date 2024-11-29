7a221e9153d86b297b454a057347de43cbd827a8
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:05:47 +0300

    cleaned SNLearnerAttendanceRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SNLearnerAttendanceRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SNLearnerAttendanceRepository.java
@@ -29,42 +29,4 @@ public interface SNLearnerAttendanceRepository extends JpaRepository<SNLearnerAt
             """)
     List<SNLearnerAttendance> allByDate_School(LocalDate attendanceDate, String schoolId);
 
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNot(Status status);
-//
-//    Optional<SNLearnerAttendance> findByStatusNotAndId(Status status , String id);
-//
-//    Optional<SNLearnerAttendance> findByStatusNotAndSchoolClass_IdAndAttendanceDate(Status status , String classId , LocalDate date);
-//    boolean existsByStatusNotAndSchoolClass_IdAndAttendanceDate(Status status , String classId , LocalDate date);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAcademicTerm_IdAndSchoolClass_School_IdAndAttendanceDate(Status status , String termId , String schoolId , LocalDate attendanceDate);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAcademicTerm_IdAndSchoolClass_School_Id(Status status , String termId , String schoolId);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndSchoolClass_School_Id(Status status ,  String schoolId);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAcademicTerm_Id(Status status , String termId);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAcademicTerm_IdAndSchoolClass_School_District_Region_Id(Status status , String termId , String regionId);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAttendanceDateAndSchoolClass_School_District_Id(Status status , LocalDate attendanceDate  , String districtId);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAcademicTerm_AcademicYear_IdAndSchoolClass_School_Id(Status status , String yearId , String schoolId);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndSchoolClass_School_IdAndAttendanceDate(Status deleted, String schoolId, LocalDate attendanceDate);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndSchoolClass_School_IdAndAttendanceDateBetween(Status deleted, String schoolId, LocalDate startDate ,LocalDate endDate);
-//
-//    @EntityGraph(value = "snlearnerattendance-schoolclass-academicterm-staff-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<SNLearnerAttendance> findAllByStatusNotAndAttendanceDate(Status deleted, LocalDate attendanceDate);
-
 }
