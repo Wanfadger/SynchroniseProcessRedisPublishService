6c5a7d1c811875c4745f03bd669ee0c78d86c64c
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:11:46 +0300

    removed test services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/controllers/SynchroniseTelaDataController.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/controllers/SynchroniseTelaDataController.java
@@ -5,10 +5,11 @@ import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.dto.Syst
 import com.planetsystems.tela.api.SynchroniseProcessRedisPublishService.service.publisher.SynchronizeMobileDataService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.CacheManager;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/SynchroniseTelaData")
@@ -18,25 +19,9 @@ public class SynchroniseTelaDataController {
 
     final SynchronizeMobileDataService synchronizeMobileDataService;
 
-
-    @Qualifier("halfHourCacheManager")
-    final CacheManager cacheManager;
-
     @PostMapping
     public ResponseEntity<SystemAppFeedBack<Boolean>> synchronizeSchoolData(@RequestBody SynchronizeSchoolDataDTO dto){
         return synchronizeMobileDataService.synchronizeSchoolData(dto);
     }
 
-
-    @GetMapping("evictDistricts")
-    public String evictDistricts(){
-        evictDistrictService();
-        return "Successfully evicted";
-    }
-
-    public void evictDistrictService() {
-        synchronizeMobileDataService.evictDistricts();
-        // Optional.ofNullable(cacheManager.getCache(CacheKeys.DISTRICTS)).ifPresent((cache -> cache.evict(SimpleKey.EMPTY) ));
-        log.info("CACHE EVICTING evictDistricts");
-    }
 }
