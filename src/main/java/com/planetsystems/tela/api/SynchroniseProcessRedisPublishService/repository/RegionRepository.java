face6e2e3dcdb105881c1272bbe8e2862c257101
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:02:59 +0300

    cleaned RegionRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/RegionRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/RegionRepository.java
@@ -10,14 +10,4 @@ import org.springframework.stereotype.Repository;
 public interface RegionRepository extends JpaRepository<Region, String> {
 	
     boolean existsByStatusNotAndNameIgnoreCase(Status status , String name);
-    
-//    @EntityGraph(value = "region-district-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<RegionProjection> findByStatusNotAndNameIgnoreCase(Status status , String name);
-//    List<RegionProjection> findAllByStatusNot(Status status);
-//    Optional<RegionProjection> findByStatusNotAndId(Status status , String id);
-//    Optional<RegionProjection> findByStatusNotAndCodeIgnoreCase(Status deleted, String code);
-//
-//
-//    @EntityGraph(value = "region-district-graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    List<RegionWithDistrictProjection> getAllByStatusNot(Status status);
 }
