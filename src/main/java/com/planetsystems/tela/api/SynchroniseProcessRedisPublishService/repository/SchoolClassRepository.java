0fa6ccca3e74559d7a3a700eaaba5b902683084a
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 10:03:38 +0300

    cleaned SchoolClassRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SchoolClassRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/SchoolClassRepository.java
@@ -10,32 +10,5 @@ import java.util.List;
 
 @Repository
 public interface SchoolClassRepository extends JpaRepository<SchoolClass, String> {
-//        List<SchoolClass> findAllByStatusNot(Status status);
-//    List<SchoolClass> findAllByStatus(Status status);
-//
-//    Optional<SchoolClass> findByStatusNotAndId(Status status , String id);
-//
-//
-//    boolean existsByStatusNotAndNameIgnoreCaseAndAcademicTerm_IdAndSchool_Id(Status status ,String name, String termId , String schoolId);
-//
-//    Optional<SchoolClass> findByStatusNotAndNameIgnoreCaseAndAcademicTerm_IdAndSchool_Id(Status status ,String name, String termId , String schoolId);
-//
-//    Optional<SchoolClass> findByStatusNotAndAcademicTerm_IdAndSchool_Id(Status status , String termId , String schoolId);
-//
-//    List<SchoolClass> findAllByStatusNotAndAcademicTerm_StatusNotAndSchool_StatusNot(Status schoolClassStatus  ,Status termStatus  ,Status schoolStatus);
-//
-//    List<SchoolClass> findAllByStatusNotAndSchool_Id(Status schoolClassStatus  ,String schoolId);
-//    long countAllByStatusNotAndSchool_Id(Status deleted, String schoolId);
-//    long countAllByStatusNotAndAcademicTerm_IdAndSchool_Id(Status deleted, String academicTerm , String schoolId);
-//
-//    List<SchoolClass> findAllByStatusNotAndAcademicTerm_Id(Status status  ,String termId);
-//
     List<SchoolClass> findAllByStatusNotAndAcademicTerm_IdAndSchool_Id(Status schoolClassStatus  , String termId , String schoolId);
-//
-//
-//    @Query("SELECT S FROM  School  S " +
-//            "" +
-//            "")
-//   List<SchoolClass> dbFindAllBySchoolWithLearnerEnrollment(Status status , String schoolId);
-
 }
