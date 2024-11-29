c98fa46cdb84da4945b7682085ed5726c99099d3
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 09:29:29 +0300

    cleaned AcademicTermRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/AcademicTermRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/AcademicTermRepository.java
@@ -12,50 +12,9 @@ import java.util.Optional;
 @Repository
 public interface AcademicTermRepository extends JpaRepository<AcademicTerm, String> {
 
-//    @EntityGraph(attributePaths = "{academicYear}" , type = EntityGraph.EntityGraphType.FETCH)
     @Query(value = """
-   SELECT A FROM AcademicTerms AS A 
-   JOIN FETCH A.academicYear AS Y
+   SELECT A FROM AcademicTerms AS A JOIN FETCH A.academicYear AS Y
    WHERE A.status <> 8 AND A.activationStatus =:activationStatus
 """)
     Optional<AcademicTerm> activeAcademicTerm(Status activationStatus);
-
-
-//    @EntityGraph(value = "academicTerm-year-Graph", type =EntityGraph.EntityGraphType.FETCH )
-//    List<AcademicTermWithYearProjection> findAllByStatusNot(Status status);
-//
-//    @EntityGraph(value = "academicTerm-year-Graph", type = EntityGraph.EntityGraphType.FETCH)
-//    List<AcademicTermWithYearProjection> findAllByStatus(Status status);
-//
-////    @EntityGraph(value = "partial-academicTerm-Graph" , type = EntityGraph.EntityGraphType.FETCH)
-//
-//    @EntityGraph(value = "academicTerm-year-Graph", type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<AcademicTermWithYearProjection> findByStatusNotAndId(Status status, String id);
-//
-////    @EntityGraph(value = "partial-academicTerm-Graph" , type = EntityGraph.EntityGraphType.FETCH)
-//
-//    @EntityGraph(value = "academicTerm-year-Graph", type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<AcademicTermWithYearProjection> findByStatusNotAndCodeIgnoreCase(Status status, String code);
-//
-//    // checks if term exits by year , name and status
-//
-//    boolean existsByStatusNotAndTermAndAcademicYear_Id(Status termStatus, String term, String yearId);
-//
-////    @EntityGraph(value = "partial-academicTerm-Graph" , type = EntityGraph.EntityGraphType.FETCH)
-//
-//    @EntityGraph(value = "academicTerm-year-Graph", type = EntityGraph.EntityGraphType.FETCH)
-//    List<AcademicTermWithYearProjection> findAllByStatusNotAndAcademicYear_Id(Status status, String yearId);
-//
-//    //    @EntityGraph(value = "partial-academicTerm-Graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    @EntityGraph(value = "academicTerm-year-Graph", type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<AcademicTermWithYearProjection> findByActivationStatusAndStatusNot(Status activattionStatus, Status status);
-//
-////    Optional<AcademicTermProjection> findByStatusNotAndActivationStatus(Status status, Status activattionStatus);
-//
-//    @Query(value = "select id from \"AcademicTerms\" s where \"status\" <> 8 and \"activationStatus\"=6", nativeQuery = true)
-//    Optional<String> findActiveAcademicTermIdByStatusNotDeleted();
-//
-//    //    @EntityGraph(value = "partial-academicTerm-Graph" , type = EntityGraph.EntityGraphType.FETCH)
-//    @EntityGraph(value = "academicTerm-year-Graph", type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<AcademicTermWithYearProjection> findByStatusNotAndCodeIgnoreCaseAndAcademicYear_CodeAndAcademicYear_StatusNot(Status termStatus, String termCode, String yearCode, Status yearStatus);
 }
