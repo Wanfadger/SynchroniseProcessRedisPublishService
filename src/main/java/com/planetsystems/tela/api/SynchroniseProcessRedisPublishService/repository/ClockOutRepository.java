3ef92ee4e43074543fcc6c28966b184013eff916
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-29 09:58:48 +0300

    cleaned ClockOutRepository by removing commented query services

--- src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/ClockOutRepository.java
+++ src/main/java/com/planetsystems/tela/api/SynchroniseProcessRedisPublishService/repository/ClockOutRepository.java
@@ -45,38 +45,4 @@ public interface ClockOutRepository extends JpaRepository<ClockOut, String> {
             """)
     List<ClockOut> allByDate_SchoolWithStaff(LocalDate clockOutDate, String schoolId);
 
-
-//    @EntityGraph(value = "clockOut-clockIn-graph", type = EntityGraph.EntityGraphType.FETCH)
-//    List<ClockOut> findAllByStatusNotAndClockIn_AcademicTerm_IdAndClockIn_School_IdAndClockOutDateOrderByClockOutDateAsc(Status status, String academicTermId, String schoolId, LocalDate clockOutDate);
-//
-//    @EntityGraph(value = "clockOut-clockIn-graph", type = EntityGraph.EntityGraphType.FETCH)
-//    Optional<ClockOut> findByStatusNotAndClockIn_SchoolStaff_IdAndClockOutDate(Status status, String StaffId, LocalDate clockOutDate);
-//
-//    @EntityGraph(value = "clockOut-clockIn-graph", type = EntityGraph.EntityGraphType.FETCH)
-//    List<ClockOut> findAllByStatusNotAndClockIn_SchoolStaff_IdAndClockOutDateBetween(Status status, String StaffId, LocalDate from, LocalDate to);
-//
-//    @EntityGraph(value = "clockOut-clockIn-graph", type = EntityGraph.EntityGraphType.FETCH)
-//    List<ClockOut> findAllByStatusNotAndClockOutDate(Status status, LocalDate date);
-//
-//    Optional<ClockOut> findByStatusNotAndId(Status status, String clockOutId);
-//
-//    boolean existsByStatusNotAndClockOutDateAndClockIn_Id(Status status, LocalDate clockOutDate, String clockInId);
-//
-//
-//    boolean existsByStatusNotAndClockIn_Id(Status status, String clockInId);
-//
-//    boolean existsByStatusNotAndClockIn_IdAndClockOutDate(Status status, String clockInId, LocalDate localDate);
-//
-//
-//    @Query(nativeQuery = true,
-//            value =
-//                    "select C.id as id " +
-//                            "from \"ClockOuts\" C \n" +
-//                            "where \n" +
-//                            "C.\"clockIn_id\"=:clockInId \n" +
-//                            "AND C.\"status\" !=:#{#status.ordinal()}\n" +
-//                            "AND C.\"clockOutDate\" =:date \n")
-//    List<String> getStaffClockOutByDate(@Param("status") Status status, @Param("clockInId") String clockInId, @Param("date") LocalDate date);
-
-
 }
